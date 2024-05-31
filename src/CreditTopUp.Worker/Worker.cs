b8347a558c21408a0d7ac23760d3c90f8582d686
Cesar M. Urdaneta <upcesar@gmail.com>
2024-05-31 11:37:24 -0300

    Worker adjustments

--- src/CreditTopUp.Worker/Worker.cs
+++ src/CreditTopUp.Worker/Worker.cs
@@ -1,15 +1,10 @@
-using MassTransit;
-
 namespace CreditTopUp.Worker;
 
-public class Worker(IBusControl busControl, ILogger<Worker> logger) : BackgroundService
+public class Worker(ILogger<Worker> logger) : BackgroundService
 {
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         logger.LogInformation("Top up consumer running");
-
-        //await busControl.StartAsync(stoppingToken);
-
-        //stoppingToken.Register(() => busControl.Stop());
+        await Task.CompletedTask;
     }
 }
